0a952ed3fe7bd712ef363bea30653f537c506f49
Sr5x4FC3s <32333361+Sr5x4FC3s@users.noreply.github.com>
2020-03-04 18:15:27 -0800

    removed node-webhooks package

--- src/components/LaunchDarklySDK/serverSDKLaunchDarkly.js
+++ src/components/LaunchDarklySDK/serverSDKLaunchDarkly.js
@@ -6,10 +6,6 @@ const ServerSDKLaunchDarkly = () => {
     <div id='server-sdk'>
       <h1>Server</h1>
       <h3>Update: [3/3/2020]: Node Server Automatically Detects Changes LaunchDarkly Site-Under-Maintenance Feature Changes And Sends Updates To PagerDuty Manager On Duty.</h3>
-      <button 
-        id='LD-sdk-button'
-        onClick={() => httpsPOST('/serverSDKFlags', {})}
-      >Test Server SDK Flag Retrieval</button>
     </div>
   )
 };

e1396fb6861e0aa5bc0957241fd68b6f26be9575
Sr5x4FC3s <32333361+Sr5x4FC3s@users.noreply.github.com>
2020-03-16 10:29:06 -0700

    added form to configure user object with custom settings to test

--- src/components/LaunchDarklySDK/sdkFeatureTestingComponent.js
+++ src/components/LaunchDarklySDK/sdkFeatureTestingComponent.js
@@ -14,12 +14,21 @@ const topicData = [
 ];
 
 const SDKFeatureTestingComponent = () => {
-  // const ldClient = LDClient;
+  const ldClient = LDClient;
+
+  const [ipAddress, setIpAddress] = useState('');
+  const [key, setKey] = useState('');
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [email, setEmail] = useState('');
+  const [country, setCountry] = useState('');
+  const [anonymous, setAnonymous] = useState(false);
+
   // test bootstrapping - local storage and server-side
 
   useEffect(() => {
     // Initialize before the component mounts
-    // ldClient.initialize(user1.ClientSideId, user1.info, {bootstrap: ''});
+    ldClient.initialize(user1.ClientSideId, user1.info, {bootstrap: ''});
 
     // End the connection after it has completely loaded all the bootstrapped features.
   }, []);
@@ -32,7 +41,78 @@ const SDKFeatureTestingComponent = () => {
           <li id={topic}><h4>{topic}</h4></li>
         </ul>
       ))}
-
+      <h2>Configure User Data and Options</h2>
+      <div>Anonymous (On/Off)</div>
+      {!anonymous ? 
+        <div style={{width: '100px', height: '50px', backgroundColor: 'red'}}>Anonymous Indicator: {anonymous}</div>
+        : 
+        <div style={{width: '100px', height: '50px', backgroundColor: 'green'}}>Anonymous Indicator: {anonymous}</div>
+      }
+      <button               
+        onClick={() => setAnonymous(!anonymous)}
+      >Toggle
+      </button>
+      <form>
+        <label>
+          IP Address
+          <input 
+            type='text'
+            onChange={e => setIpAddress(e.target.value)}
+            placeholder='IP Address'
+          />
+        </label>
+        <label>
+          Key
+          <input 
+            type='text'
+            onChange={e => setKey(e.target.value)}
+            placeholder='Key'
+          />
+        </label>
+        <label>
+          First Name
+          <input 
+            type='text'
+            onChange={e => setFirstName(e.target.value)}
+            placeholder='First Name'
+          />
+        </label>
+        <label>
+          Last Name
+          <input 
+            type='text'
+            onChange={e => setLastName(e.target.value)}
+            placeholder='Last Name'
+          />
+        </label>
+        <label>
+          Email
+          <input 
+            type='text'
+            onChange={e => setEmail(e.target.value)}
+            placeholder='Email'
+          />
+        </label>
+        <label>
+          Country
+          <input 
+            type='text'
+            onChange={e => setCountry(e.target.value)}
+            placeholder='Country'
+          />
+        </label>
+        <label>
+          Anonymous
+          <button               
+            onClick={() => setAnonymous(!anonymous)}
+          >Toggle
+          </button>
+        </label>
+        <input 
+          type='submit'
+          value='submit'
+        />
+      </form>
     </div>
   )
 };
